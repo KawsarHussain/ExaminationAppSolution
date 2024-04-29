f4a8d6d283df9dfa85f02efeebd08c3af4525faf
KawsarHussain <kawsar.hussain.365@gmail.com>
2024-04-29 10:12:50 +0100

    Add project files.

---
+++ ExaminationApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ExaminationApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
