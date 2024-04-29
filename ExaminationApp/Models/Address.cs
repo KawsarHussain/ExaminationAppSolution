410183df03bf0a888f41daa4b0a611d7102c6159
KawsarHussain <kawsar.hussain.365@gmail.com>
2024-04-29 10:50:58 +0100

    Created a user class and Address struct with getters and setters

---
+++ ExaminationApp/Models/Address.cs
@@ -0,0 +1,42 @@
+﻿namespace ExaminationApp.Models;
+
+internal struct Address
+{
+    #region Attributes
+
+    private string _doorNumber;
+    private string _firstAddressLine;
+    private string _postCode;
+    private string _city;
+
+    #endregion
+
+
+    #region Getters and Setters
+
+    public string DoorNumber
+    {
+        get { return this._doorNumber; }
+        set { this._doorNumber = value; }
+    }
+
+    public string FirstAddressLine
+    {
+        get { return this._firstAddressLine; }
+        set { this._firstAddressLine = value;}
+    }
+
+    public string PostCode
+    {
+        get { return this._postCode; }
+        set { this._postCode = value; }
+    }
+
+    public string City
+    {
+        get { return this._city; }
+        set { this._city = value; }
+    }
+
+    #endregion
+}
---
+++ ExaminationApp/Models/User.cs
@@ -0,0 +1,73 @@
+﻿namespace ExaminationApp.Models;
+
+internal class User
+{
+    #region Attributes
+
+    private int _userId;
+    private string _firstName;
+    private string _lastName;
+    private string? _otherName;
+    private string _title;
+    private string _emailAddress;
+    private bool _verifiedEmail;
+    private string _telephoneNumber;
+    private Address _address;
+
+    #endregion
+
+
+    #region Getters and Setters
+
+    public int UserId
+    {
+        get { return this._userId; } 
+    }
+
+    public string FirstName
+    {
+        get { return this._firstName; }
+        set { this._firstName = value; }
+    }
+
+    public string LastName
+    {
+        get { return this._lastName; }
+        set { this._lastName = value; }
+    }
+
+    public string? OtherName
+    {
+        get { return this._otherName; }
+        set { this._otherName = value; }
+    }
+
+    public string Title
+    {
+        get { return this._title; }
+        set { this._title = value; }
+    }
+
+    public string EmailAddress
+    {
+        get { return this._emailAddress; }
+    }
+
+    public bool VerifiedEmail
+    {
+        get { return this._verifiedEmail; }
+    }
+
+    public string TelephoneNumber
+    {
+        get { return this._telephoneNumber; }
+        set { this._telephoneNumber = value; }
+    }
+
+    public Address Address
+    {
+        get { return this._address; }
+    }
+
+    #endregion
+}
